03f87d5890684823c208a128d362590b2bcd975d
Marika <m.abashidze@ssu.edu.ge>
2021-07-31 04:28:11 +0400

    lesson-17

---
+++ script.js
@@ -0,0 +1,111 @@
+//I
+// let numbers = [5, 25, 89, 120, 36];
+// console.log(numbers.length);
+// numbers.push('javascript', 'python');
+// numbers.unshift('html','css');
+// numbers.log(names);
+
+// names.shift();
+// names.pop();  
+// console.log(names);
+
+
+
+
+
+//II
+// let names = ['orange', 'banana', 'pear'];
+// console.log(names.length);
+// names.push('apple','pineapple');
+// names.unshift('watermelon');
+// names.splice(2,0, 'mango');
+// names.shift();
+// names.pop();
+// console.log(names);
+
+
+
+
+
+//III
+// let user = {
+//     name: 'megi',
+//     age: 22,
+//     gender:'male'
+// };
+// for (let x of Object.keys(user)){
+//     console.log(x);
+// }
+
+// Object.keys(user).forEach(function(x){
+//     console.log(x);
+// })
+
+
+
+
+
+
+//IV
+// let user = {
+//     name: 'megi',
+//     age: 22,
+//     gender:'female'
+// };
+
+
+// for (let x of Object.values(user)){
+//     console.log(x);
+// }
+
+// Object.values(user).forEach(function(x){
+//     console.log(x);
+// })
+
+
+
+
+
+
+//V
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// let newArray = array.map( (x) => x / 3 );
+// console.log(newArray);
+
+
+// let newArray = array.map(function(x){
+//     return x / 3;
+// })
+// console.log(newArray);
+
+
+
+
+//VI
+
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let newArray = array.filter((y) => typeof y == 'number');
+// console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
